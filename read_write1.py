375a34e13c7f573eb08ce5fabceb60fb6d21696e
YeganeGholipour <yeganegholiour@gmail.com>
2024-05-16 12:44:24 +0330

    add : serial connection for camera - just Blue

--- read_write1.py
+++ read_write1.py
@@ -1,7 +1,11 @@
 import os
 import time
 import cv2
+import socket
+import pickle
+from dynamixel_sdk import *
 
+# Check platform and set getch function for user input
 if os.name == 'nt':
     import msvcrt
     def getch():
@@ -18,36 +22,30 @@ else:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-from dynamixel_sdk import *                    
-from color_detection_calss import ColorDetector   
+# Initialize Dynamixel SDK constants and variables
+ADDR_MX_TORQUE_ENABLE = 24
+ADDR_MX_GOAL_POSITION = 30
+ADDR_MX_PRESENT_POSITION = 36
+ADDR_MOVING_SPEED = 32
 
+PROTOCOL_VERSION = 1.0
+DXL_ID = 4
+BAUDRATE = 57600
+DEVICENAME = '/dev/tty0'
 
-ADDR_MX_TORQUE_ENABLE      = 24               
-ADDR_MX_GOAL_POSITION      = 30
-ADDR_MX_PRESENT_POSITION   = 36
-ADDR_MOVING_SPEED          = 32
-
-PROTOCOL_VERSION            = 1.0               
-
-DXL_ID                      = 4             
-BAUDRATE                    = 57600            
-DEVICENAME                  = '/dev/tty0'   
-                                                
-
-TORQUE_ENABLE               = 1                
-TORQUE_DISABLE              = 0                
-DXL_MINIMUM_POSITION_VALUE  = 10         
-DXL_MAXIMUM_POSITION_VALUE  = 4000          
-DXL_MOVING_STATUS_THRESHOLD = 20              
+TORQUE_ENABLE = 1
+TORQUE_DISABLE = 0
+DXL_MINIMUM_POSITION_VALUE = 10
+DXL_MAXIMUM_POSITION_VALUE = 4000
+DXL_MOVING_STATUS_THRESHOLD = 20
 
 index = 0
-dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]
 
 portHandler = PortHandler(DEVICENAME)
-
-
 packetHandler = PacketHandler(PROTOCOL_VERSION)
 
+# Open port
 if portHandler.openPort():
     print("Succeeded to open the port")
 else:
@@ -56,7 +54,7 @@ else:
     getch()
     quit()
 
-
+# Set baudrate
 if portHandler.setBaudRate(BAUDRATE):
     print("Succeeded to change the baudrate")
 else:
@@ -65,14 +63,12 @@ else:
     getch()
     quit()
 
-
+# Enable Dynamixel torque and set initial positions
 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MOVING_SPEED, 50)
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, 1, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 1, ADDR_MX_GOAL_POSITION, 2000)
 dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 12, ADDR_MX_GOAL_POSITION, 24000)
-
-
 
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
@@ -81,73 +77,80 @@ elif dxl_error != 0:
 else:
     print("Dynamixel has been successfully connected")
 
+# Socket setup for receiving color detection messages
+port = 12345
+s = socket.socket()
+s.bind(('192.168.1.100', port))
+s.listen(5)
+print("Waiting for connection from the client...")
+client, addr = s.accept()
+print("Connected to client at address:", addr)
 
+try:
+    while True:
+        print("Press any key to continue! (or press ESC to quit!)")
 
-while 1:
-    print("Press any key to continue! (or press ESC to quit!)")
+        dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, dxl_goal_position[index])
 
-    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, dxl_goal_position[index])
-    # ****************************************************************************
-    detector = ColorDetector()
-    cv2.namedWindow("Camera Feed")
-    # ****************************************************************************
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-    while 1:
-        # Read present position
-        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
-        
-        # Check if goal position is not reached
-        if  abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
+        while True:
+            # Read present position
+            dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-            ret, frame = detector.cap.read()
-            if not ret:
+            print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
+
+            # Check if goal position is not reached
+            if abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
+                try:
+                    m = pickle.dumps(["salam"])
+                    # Check for messages from the client
+                    mes = client.recv(1024)
+                    if mes:
+                        message = pickle.loads(mes)
+                        if message == ["blue"]:
+                            print("Detected Color is Blue")
+                            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 1, ADDR_MX_GOAL_POSITION, 1500)
+                            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 12, ADDR_MX_GOAL_POSITION, 18000)
+                            # pick and place
+                            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 1, ADDR_MX_GOAL_POSITION, 1200)
+                            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 12, ADDR_MX_GOAL_POSITION, 20000)
+                            time.sleep(3)
+                            break
+                except Exception as e:
+                    print("Error receiving data:", e)
+
+                cv2.waitKey(1)
+
+            else:
+                print("Goal position reached. Stopping...")
                 break
 
-            roi, x1, y1 = detector.calculate_roi(frame)
-            detector.draw_rectangle(roi, x1, y1, frame)
-            hsv_frame = detector.transform_to_hsv(roi)
+        print("Scanning complete.")
 
-            blue_count, green_count, red_count = detector.calculate_bounds(hsv_frame)
-            color = detector.show_color(blue_count, green_count, red_count, x1, y1, frame)
-
-            cv2.imshow("Camera Feed", frame)  
-
-            if color == "Blue":
-                print("Detected Color is Blue")
-                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 1, ADDR_MX_GOAL_POSITION, 1500)
-                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, 12, ADDR_MX_GOAL_POSITION, 18000)
-                time.sleep(3)
-                break
-
-            cv2.waitKey(1)
-
+        if index == 0:
+            index = 1
         else:
-            print("Goal position reached. Stopping...")
-            break
-            
-    detector.cap.release()
-    cv2.destroyAllWindows()
-    print("Scanning complete.")                
+            index = 0
 
-    if index == 0:
-        index = 1
-    else:
-        index = 0
+except KeyboardInterrupt:
+    print("Terminating...")
 
+finally:
+    # Close socket connection
+    client.close()
+    s.close()
 
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-portHandler.closePort()
+    portHandler.closePort()
---
+++ untitled0.py
@@ -0,0 +1,137 @@
+import socket
+import pickle
+import cv2
+import numpy as np
+
+class ColorDetector:
+    def __init__(self):
+        self.rect_color = (255, 255, 255)
+        self.rect_thickness = 2
+
+        self.cap = cv2.VideoCapture(1)
+        
+        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
+        self.rect_width = 200
+        self.rect_height = 200
+
+    @property
+    def rectangle_width(self):
+        return self.rect_width
+        
+    @rectangle_width.setter    
+    def rectangle_width(self, value):
+        self.rect_width = value
+
+    @property
+    def rectangle_height(self):
+        return self.rect_height
+        
+    @rectangle_height.setter    
+    def rectangle_height(self, value):
+        self.rect_height = value
+
+    def calculate_roi(self, frame):
+        x1 = int((self.frame_width - self.rect_width) / 2)
+        y1 = int((self.frame_height - self.rect_height) / 2)
+        roi = frame[y1: y1 + self.rect_height, x1:x1 + self.rect_width]
+        return (roi, x1, y1)
+
+    def draw_rectangle(self, roi, x1, y1, frame):
+        cv2.rectangle(frame, (x1, y1), (x1 + self.rect_width, y1 + self.rect_height), self.rect_color, self.rect_thickness)
+
+    def transform_to_hsv(self, roi):
+        hsv_frame = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        return hsv_frame
+
+    def calculate_bounds(self, hsv_frame):
+        blue_lower_bound = np.array([100, 100, 100])
+        blue_upper_bound = np.array([140, 255, 255])
+    
+        green_lower_bound = np.array([40, 100, 100])
+        green_upper_bound = np.array([80, 255, 255])
+    
+        red_lower_bound1 = np.array([0, 100, 100])
+        red_upper_bound1 = np.array([10, 255, 255])
+    
+        red_lower_bound2 = np.array([160, 100, 100])
+        red_upper_bound2 = np.array([180, 255, 255])
+
+        mask_blue = cv2.inRange(hsv_frame, blue_lower_bound, blue_upper_bound)
+        mask_green = cv2.inRange(hsv_frame, green_lower_bound, green_upper_bound)
+        mask_red1 = cv2.inRange(hsv_frame, red_lower_bound1, red_upper_bound1)
+        mask_red2 = cv2.inRange(hsv_frame, red_lower_bound2, red_upper_bound2)
+        mask_red = cv2.bitwise_or(mask_red1, mask_red2)
+    
+        contours_blue, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_green, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_red, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        blue_contour_count = len(contours_blue)
+        green_contour_count = len(contours_green)
+        red_contour_count = len(contours_red)
+
+        return (blue_contour_count, green_contour_count, red_contour_count)
+
+
+    def show_color(self, blue_contour_count, green_contour_count, red_contour_count, x1, y1, frame):
+        max_contour_count = max(blue_contour_count, green_contour_count, red_contour_count)
+        
+        if max_contour_count == blue_contour_count:
+            color = "Blue"
+            cv2.putText(frame, color, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        
+        elif max_contour_count == green_contour_count:
+            color = "Green"
+            cv2.putText(frame, color, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        
+        else:
+            color = "Red"
+            cv2.putText(frame, color, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+        
+        return color
+
+port = 12345
+port1=12346	
+s = socket.socket()		
+s.connect(('192.168.1.100', port)) 
+s1=socket.socket()	
+s1.bind(('192.168.1.102',port1))
+s1.listen(5)
+client,addr = s1.accept()
+initial_info=s.recv(1024)
+
+m=pickle.loads(initial_info)
+if m==['salam']:
+    detector = ColorDetector()  
+    while True:
+        ret, frame = detector.cap.read()
+        if not ret:
+            break
+
+        roi, x1, y1 = detector.calculate_roi(frame)
+        detector.draw_rectangle(roi, x1, y1, frame)
+        hsv_frame = detector.transform_to_hsv(roi)
+
+        blue_count, green_count, red_count = detector.calculate_bounds(hsv_frame)
+        color = detector.show_color(blue_count, green_count, red_count, x1, y1, frame)
+
+        cv2.imshow("Color Detection", frame)
+
+        if color == "Blue":
+            mes=pickle.dumps(["blue"])
+            client.send(mes)
+            print("Detected Color is Blue")
+        else:
+            print("Detected Color is Not Blue")
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    detector.cap.release()
+    cv2.destroyAllWindows()
+else:
+    print("not connected")
+
+#print(f"message: {m}")
